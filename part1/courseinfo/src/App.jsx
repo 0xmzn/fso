15599ff88cdaa5031284fa055237066804684ce3
Mazen Saad <mmes3666@gmail.com>
2023-10-18 15:19:11 +0200

    Update part1/courseinfo

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -14,8 +14,8 @@ const Part = (props) => {
 const Content = (props) => {
     return (
         <div>
-            {props.data.map( (e, index) => 
-                <Part key={index} part={e.name} exercises={e.exercises}/>
+            {props.data.map((e, index) =>
+                <Part key={index} part={e.name} exercises={e.exercises} />
             )}
         </div>
     );
@@ -28,27 +28,29 @@ const Footer = (props) => {
 };
 
 const App = () => {
-    const course = 'Half Stack application development';
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        }
-    ];
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    };
 
     return (
         <div>
-            <Header course={course} />
-            <Content data={parts}/>
-            <Footer />
+            <Header course={course.name} />
+            <Content data={course.parts} />
+            <Footer sum={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} />
         </div>
     );
 };
